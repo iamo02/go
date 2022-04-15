eddf0bec2349cb52ba1176ebf9ee87d8191e9b3d
Pongchai Boonmee <iamo.java@gmail.com>
2022-04-15 16:41:37 +0700

    delete main

--- stock/go.mod
+++ stock/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/itsjamie/gin-cors v0.0.0-20220228161158-ef28d3d2a0a8
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	gorm.io/driver/sqlite v1.1.5 // indirect
 	gorm.io/gorm v1.21.15 // indirect
--- stock/service.go
+++ stock/service.go
@@ -4,12 +4,28 @@ import (
 	"fmt"
 	"main/api"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	cors "github.com/itsjamie/gin-cors"
 )
 
 func main() {
 	r := gin.Default()
+
+	// Set up CORS middleware options
+	config := cors.Config{
+		Origins:        "*",
+		RequestHeaders: "Origin, Authorization, Content-Type",
+
+		Methods:         "GET, POST, PUT, DELETE",
+		Credentials:     true,
+		ValidateHeaders: false,
+		MaxAge:          1 * time.Minute,
+	}
+
+	r.Use(cors.Middleware(config))
+
 	r.Static("/images", "./uploaded/images")
 	api.Sutup(r)
 	//	r.Run(":81")
