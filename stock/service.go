769ee12c2ae966e98d6a13a84723357123b3da17
Pongchai Boonmee <iamo.java@gmail.com>
2022-04-15 14:16:25 +0700

    delete main

--- stock/service.go
+++ stock/service.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"main/api"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,5 +12,15 @@ func main() {
 	r := gin.Default()
 	r.Static("/images", "./uploaded/images")
 	api.Sutup(r)
-	r.Run(":81")
+	//	r.Run(":81")
+
+	//	In case of running on Heroku
+	var port = os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("Running on Heroku using random PORT")
+		r.Run()
+	} else {
+		fmt.Println("Environment Port : " + port)
+		r.Run(":" + port)
+	}
 }
